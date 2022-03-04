bca79fcd8dcf7c775fbcf1f9c0d44d43c3a4e6e9
binarygit <inscription4@outlook.com>
2022-03-04 14:43:20 +0545

    add initial styles

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("Hello!")
+let ho = document.getElementById('ho')
+let haina = document.getElementById('haina')
+console.log(ho, haina)
+
+ho.addEventListener('click', insult)
+
+function insult() {
+  console.log('insult')
+}
